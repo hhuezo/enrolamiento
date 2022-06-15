17fea1a2af82104b98adffe516b9970fcae0bc7c
hhuezo <hugo.alex.huezo@gmail.com>
2022-06-15 10:15:10 -0600

    redirection options

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OptionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+  }
 
   ngOnInit(): void {
+  }
+
+  datos_personales() {
+    this.router.navigate(['/personal-information']);
+  }
+
+  datos_fisicos() {
+    this.router.navigate(['/physic-information']);
+  }
+
+  datos_demograficos() {
+    this.router.navigate(['/demographic-information']);
+  }
+
+  fotografia() {
+    this.router.navigate(['/photography']);
+  }
+
+  firma() {
+    this.router.navigate(['/sign']);
+  }
+
+  huella() {
+    this.router.navigate(['/fingerprint']);
   }
 
 }
